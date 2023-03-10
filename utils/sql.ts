a3161156060d574493e2bbe8a9589412b8b96bcf
lizhang <2475934003@qq.com>
2023-03-10 19:00:45 +0800

    add sql util

--- utils/sql.ts
+++ utils/sql.ts
@@ -1,6 +1,11 @@
 import { Utils } from '.'
 import { escape } from '../db/mysql'
 
+/**
+ * 根据参数对象获取sql条件条件语句
+ * @param param 搜索条件参数对象
+ * @returns sql条件语句
+ */
 export const getCondition = <T extends object>(param: T) => {
   return Object.keys(param).reduce<string>((res, key, index, arr) => {
     const value = Utils.isValidKey(key, param) && param[key]
