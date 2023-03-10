9ffee83eb1b2b12889dbf92022702ece9fc7f926
lizhang <2475934003@qq.com>
2023-03-10 19:18:18 +0800

    add blogList type

--- db/blog.ts
+++ db/blog.ts
@@ -1,7 +1,13 @@
 import { SearchBlogParam } from '../types/blog'
+import { BlogList } from '../types/db'
 import { getCondition } from '../utils/sql'
 import { runSQL } from './mysql'
 
+/**
+ * 获取文章列表
+ * @param param 搜索参数
+ * @returns 列表
+ */
 export const getBlogList = async (param: SearchBlogParam) => {
   const condition = getCondition<SearchBlogParam>(param)
   const sql = `
@@ -9,7 +15,7 @@ export const getBlogList = async (param: SearchBlogParam) => {
   FROM blogs
   WHERE ${condition}
   `
-  return await runSQL(sql)
+  return await runSQL<BlogList>(sql)
 }
 
 export const blogDB = {
