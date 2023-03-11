fff67759e9c4cd04b04f93c758ce9faeaccc86eb
lizhang <2475934003@qq.com>
2023-03-12 01:51:35 +0800

    fix blog list query

--- config/sys.ts
+++ config/sys.ts
@@ -1,5 +1,5 @@
 export const sysConfig = {
-  TOKEN_EXPIRES: 20,
+  TOKEN_EXPIRES: 30 * 60,
   SECRET_KEY: 'brandnewzhang2023-03-06',
   PORT: 3001
 }
--- controller/blog.ts
+++ controller/blog.ts
@@ -2,11 +2,12 @@ import { blogDB } from '../db/blog'
 import { Result } from '../res/result'
 import { BlogController } from '../types/blog'
 import { BlogList } from '../types/db'
+import { logger } from '../utils'
 
 export const blogController: BlogController = {
   async getBlog(req, res, next) {
-    const body = req.body
-    const data = await blogDB.getBlogList(body)
+    const query = req.query
+    const data = await blogDB.getBlogList(query)
     res.json(Result.ok<BlogList>(data))
     return
   }
